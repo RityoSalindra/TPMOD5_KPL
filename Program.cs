02eeecb919002ed7435be65718a979ccf3d8f802
Rityo Salindra <nuruddin859@gmail.com>
2022-03-25 15:44:01 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo a = new SayaTubeVideo("Tutorial Design By Contract – [Nuruddin Rityo Salindra]");
+            a.PrintVideoDetails();
+            a.IncreasePlayCount(2);
+            a.PrintVideoDetails();
+        }
+    }
+
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string a)
+        {
+            Random b = new Random();
+
+            this.id = b.Next(10000, 100000);
+            this.title = a;
+            playCount = 0;
+
+        }
+
+        public void IncreasePlayCount(int a)
+        {
+            playCount += a;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("id :" + id);
+            Console.WriteLine("Title :" + title);
+            Console.WriteLine("PlayCount : " + playCount);
+            Console.WriteLine();
+        }
+    }
+}
